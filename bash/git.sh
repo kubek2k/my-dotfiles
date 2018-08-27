b38b895658a864e11fb31253e6784ad687d5db21
Jakub Janczak <jakub.janczak@schibsted.pl>
2018-08-27 22:14:12 +0200

    More automation on one-commit-pr

--- bash/git.sh
+++ bash/git.sh
@@ -9,11 +9,14 @@ function ocpr {
        echo "No message provided" > /dev/stderr
        return 1
    fi
+   OCPR_REVIEWERS_FILE="$(git rev-parse --show-toplevel)/.ocpr-reviewers" 
+   if [ -f $OCPR_REVIEWERS_FILE ]; then
+       REVIEWERS_PARAM="-r `cat $OCPR_REVIEWERS_FILE | paste -sd ',' -`"
+   fi
    BRANCH_NAME=`echo "$1" | tr '[:upper:]' '[:lower:]' | sed -e 's/[^A-Za-z]/-/g'`
    git checkout -b "$BRANCH_NAME"
    git add .
-   git commit -m "$1"
+   git commit -m "$*"
    git push origin
-   hub pull-request
+   hub pull-request -o $REVIEWERS_PARAM
 }
-
