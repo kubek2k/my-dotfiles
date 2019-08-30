7f4bf17d6aa8b00ff62401189cf1ca17d2ef58b3
Jakub Janczak <jakub.janczak@schibsted.pl>
2019-08-30 10:51:21 +0200

    Updating dash sha

--- nix/overlays/mac/dash/default.nix
+++ nix/overlays/mac/dash/default.nix
@@ -4,7 +4,7 @@
   sourceRoot = "Dash.app";
   src = fetchurl {
     url = https://kapeli.com/downloads/v4/Dash.zip;
-    sha256 = "0n429q4b8g7fmpx50zkamq19hfd1629jgj055429iw0818891sdg";
+    sha256 = "0zvc119n30ya7xja4f2ksgqcdf8c4xjzszr8w0zgm8w65nfsi8y1";
   };
   description = "Dash is an API Documentation Browser and Code Snippet Manager";
   homepage = https://kapeli.com/dash;
