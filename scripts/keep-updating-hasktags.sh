4bff91a308deed52b8ae3b818ddb81c1521c4339
Jakub Janczak <jakub.janczak@scrive.com>
2019-12-28 22:38:11 +0100

    Adding ability to specify the directories to check for haskell sources

--- scripts/keep-updating-hasktags.sh
+++ scripts/keep-updating-hasktags.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p entr fd haskellPackages.hasktags coreutils ncurses
 
-fd '.*\.(hs|lhs|hsc)$' | entr -c sh -c 'echo "Updating tags $(tput setaf 2)$(date -Iseconds)$(tput sgr0)"; hasktags -cx .'
+if [ $# -eq 0 ]; then
+    DIRS="."
+else
+    DIRS="$*"
+fi
+
+fd '.*\.(hs|lhs|hsc)$' | entr -c sh -c "echo \"Updating tags $(tput setaf 2)$(date -Iseconds)$(tput sgr0)\" && hasktags -cx $DIRS"
