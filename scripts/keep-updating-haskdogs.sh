5e634da12e64d40934c736be623121c754ff16a9
Jakub Janczak <jakub.janczak@scrive.com>
2019-12-28 23:08:28 +0100

    Adding an additional script to index also dependencies of the project

---
+++ scripts/keep-updating-haskdogs.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p entr fd ghc haskellPackages.haskdogs haskellPackages.hasktags haskellPackages.cabal-install coreutils ncurses
+
+if [ $# -eq 0 ]; then
+    DIRS="."
+else
+    DIRS="$*"
+fi
+
+fd '.*\.(hs|lhs|hsc|cabal|project)$' | entr -c sh -c "echo \"Updating tags \$(tput setaf 2)\$(date -Iseconds)\$(tput sgr0)\$0\" && 
+    if [[ \"\$0\" =~ \"cabal\" ]]; then 
+        echo \"Change in cabal files detected - invoking haskdogs\"
+        haskdogs --hasktags-args \"-cx $DIRS\" 
+    else 
+        hasktags -cx $DIRS 
+    fi" /_
